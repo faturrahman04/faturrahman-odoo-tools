5eaed6d3fc5470081247f6466f5da3db50bae6f1
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2026-02-17 23:16:22 +0700

    Add config module, so the module can be enable or disable

--- sale_generate_note/models/res_config_settings.py
+++ sale_generate_note/models/res_config_settings.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
   _inherit = 'res.config.settings'
 
-  generate_note = fields.Boolean(string="Generate Note")
+  generate_note = fields.Boolean(string="Generate Note", config_parameter="sale_generate_note.generate_note", default=False)
--- sale_generate_note/models/sale_order.py
+++ sale_generate_note/models/sale_order.py
@@ -7,35 +7,46 @@ class SaleOrder(models.Model):
 
   @api.depends('partner_id', 'payment_term_id')
   def _compute_note(self):
-    for rec in self:
-      print(rec.name)
-      if rec.partner_id and rec.payment_term_id:
-        rec.note = f"""
+    is_generated_note_enabled = self.env['ir.config_parameter'].sudo().get_param(
+      'sale_generate_note.generate_note'
+     ) == 'True'
+    if is_generated_note_enabled:
+      for rec in self:
+        print(rec.name)
+        if rec.partner_id and rec.payment_term_id:
+          rec.note = f"""
 <strong>{rec.name} Details</strong><br />
 Customer : {rec.partner_id.display_name}<br />
 Payment Term : {rec.payment_term_id.display_name}<br />
 Total : {self.env.company.currency_id.symbol} {rec.amount_total:,.2f}
 """
         
+  @api.model
+  def _get_generate_note_config(self):
+    is_generate_note_enabled = self.env['res.config.settings'].search([('')])
+        
 class SaleOrderLine(models.Model):
   _inherit = 'sale.order.line'
 
   def write(self, vals):
-    for so in self:
-      old_product_uom_qty = so.product_uom_qty
-    res = super().write(vals)
-    
-    for line in self:
-      if 'product_uom_qty' in vals:
-        self.order_id.message_post_with_source(
-          "sale_generate_note.sale_order_mail_template",
-          render_values={
-            'product':line.product_id.display_name, 
-            'quantity': line.product_uom_qty, 
-            'old_quantity' : old_product_uom_qty}
-        )
+    is_generated_note_enabled = self.env['ir.config_parameter'].sudo().get_param('sale_generate_note.generate_note', True) 
+
+    if is_generated_note_enabled:
+      for so in self:
+        old_product_uom_qty = so.product_uom_qty
+      res = super().write(vals)
       
-    return res
+      for line in self:
+        if 'product_uom_qty' in vals:
+          self.order_id.message_post_with_source(
+            "sale_generate_note.sale_order_mail_template",
+            render_values={
+              'product':line.product_id.display_name, 
+              'quantity': line.product_uom_qty, 
+              'old_quantity' : old_product_uom_qty}
+          )
+      
+    return super().write(vals)
  
 
 
